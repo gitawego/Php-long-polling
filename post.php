fc9422165d13010071ec05dd236ceea2a4770979
gitawego <gitawego@gmail.com>
2013-06-19 15:12:13 +0200

    Update post.php

--- post.php
+++ post.php
@@ -1,19 +1,18 @@
 <?php
-	// Inicia sessão
 	session_start();
-
+	$chatLog = dirname(__FILE__).'/chat.txt';
 	$name = $_SESSION['name'];
 	$post = $_POST['post'];
 	
-	// Lê o arquivo
-	$data = file_get_contents('chat.txt');
-	$lines = explode("\n", $data);
+	$data = file_get_contents($chatLog);
+	if(isset($data)){
+		$lines = explode("\n", $data);		
+	}else{
+		$lines = array();
+	}
 
-	// Verifica se é necessário usar quebra de linha
-	$line = empty($lines[0]) ? $line = "" : $line = "\n";
-	 
-	// Insere linha no arquivo e salva
+	$line = empty($lines[0]) ? "" : "\n";
 	$line = $line . "<b>" . $name . "</b>: " . $post; 
 	$data = $data . $line;
-	file_put_contents('chat.txt', $data);
-?>
+	file_put_contents($chatLog, $data);
+
